21ee51ff104089754767f825164fda22c8de80c5
breez <ilya66401@gmail.com>
2024-12-04 22:59:54 +0300

    test deploy with nginx #4

--- migration.sh
+++ migration.sh
@@ -11,4 +11,3 @@ echo "Running migrations on container ID: $FLASK_CONTAINER_ID"
 docker exec -it $FLASK_CONTAINER_ID flask db init
 docker exec -it $FLASK_CONTAINER_ID flask db migrate -m "Initial migration"
 docker exec -it $FLASK_CONTAINER_ID flask db upgrade
-echo "Database migration completed."
