9090f29bde62343a117662b4873a7a326ed908df
breez <ilya66401@gmail.com>
2024-12-04 22:53:54 +0300

    test deploy with nginx #3

--- migration.sh
+++ migration.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-FLASK_CONTAINER_ID=$(docker ps -q --filter 'ancestor=cloud-services-web')
+FLASK_CONTAINER_ID=$(docker ps -q --filter "ancestor=cloud-services-web" | head -n 1)
 
 if [ -z "$FLASK_CONTAINER_ID" ]; then
     echo "Flask container not found"
     exit 1
 fi
 
-docker exec -i $FLASK_CONTAINER_ID flask db init
-docker exec -i $FLASK_CONTAINER_ID flask db migrate -m "Initial migration"
-docker exec -i $FLASK_CONTAINER_ID flask db upgrade
+echo "Running migrations on container ID: $FLASK_CONTAINER_ID"
+docker exec -it $FLASK_CONTAINER_ID flask db init
+docker exec -it $FLASK_CONTAINER_ID flask db migrate -m "Initial migration"
+docker exec -it $FLASK_CONTAINER_ID flask db upgrade
+echo "Database migration completed."
